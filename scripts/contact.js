d3ceb2773ae2aaabe9de044fd0e632adc3e8f7c9
Zetterberg Wallin Georg <georg.zetterberg.wallin@scania.com>
2019-04-18 14:53:09 +0200

    Uppdaterade med present och transport info

--- scripts/contact.js
+++ scripts/contact.js
@@ -5,4 +5,7 @@
 element = document.getElementById('mailJonas');
 element.innerHTML = userJonas + '@' + domainJonas;
 element.href = 'mailto:' + userJonas + '@' + domainJonas;
-//}
+
+var number1 = '+4672', number2 = '7250001'
+element = document.getElementById('nummer');
+element.innerHTML = number1 + number2;
